ef8c38bd92e38b3a42e04852104d0b61ce3130c1
ramreim <studen_studen@yahoo.com>
2022-08-28 17:37:47 +0200

    papildyta labels

--- Threads002/Form1.cs
+++ Threads002/Form1.cs
@@ -12,11 +12,90 @@ namespace Threads002
 {
     public partial class Form1 : Form
     {
+
+
+        Button btnStart = new Button();
+        Button btnStop = new Button();
+        Button btnExit = new Button();
+
+        TextBox txtbThreadNr = new TextBox();
+
+
+        Label lblThreadNr = new Label();
+
+
         public Form1()
         {
+
+
+
+            this.btnExit.Click += new System.EventHandler(this.btnExit_click);
+
+
+
             InitializeComponent();
             this.Size = new Size(1400,700);
+            this.Text = "Pratimo atlikimas";
+            this.ControlBox = false;
+
+            #region labels_buttons_coordinates
+
+            btnExit.Left = 1200;
+            btnExit.Top = 20;
+            btnExit.Height = 20;
+            btnExit.Width = 100;
+            btnExit.Text = "Close";
+            btnExit.Enabled = true;
+            this.Controls.Add(btnExit);
+
+            btnStart.Left = 50;
+            btnStart.Top = 120;
+            btnStart.Height = 20;
+            btnStart.Width = 100;
+            btnStart.Text = "Start";
+            btnStart.Enabled = true;
+            this.Controls.Add(btnStart);
+
+            btnStop.Left = 50;
+            btnStop.Top = 160;
+            btnStop.Height = 20;
+            btnStop.Width = 100;
+            btnStop.Text = "Stop";
+            btnStop.Enabled = false;
+            this.Controls.Add(btnStop);
+
+
+            txtbThreadNr.Left = 200;
+            txtbThreadNr.Top = 40;
+            txtbThreadNr.Height = 20;
+            txtbThreadNr.Width = 50;
+            txtbThreadNr.Text = "2";
+            txtbThreadNr.Enabled = true;
+            this.Controls.Add(txtbThreadNr);
+
+
+
+            lblThreadNr.Left = 140;
+            lblThreadNr.Top = 40;
+            lblThreadNr.Height = 20;
+            lblThreadNr.Width = 60;
+            lblThreadNr.Text = "Saku sk.:";
+            lblThreadNr.Enabled = true;
+            this.Controls.Add(lblThreadNr);
+
+
+            
+
+            #endregion labels_buttons_coordinates
 
         }
+
+        private void btnExit_click(object sender, EventArgs e)
+        {
+
+            System.Windows.Forms.Application.Exit();
+
+        }
+
     }
 }
